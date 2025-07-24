93459d23b89e4caa5d335e8edb677025080e9952
Xiang Zhang <zhangxiang@simm.ac.cn>
2025-07-24 15:12:44 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,40 +1,9 @@
 #!/usr/bin/env python3
-"""
-FARScore: A deep learning tool for predicting molecular synthetic accessibility
-
-This package provides tools for:
-- Predicting molecular synthetic accessibility from SMILES strings
-- Training and fine-tuning models on custom datasets
-- Processing molecular data for machine learning
-"""
-
 __version__ = "1.0.0"
-__author__ = "Xiang Zhang"
-__email__ = "776206454@qq.com"
-__description__ = "FARScore' is a synthetic accessibility predictor as Python module that allows you to calculating molecules' synthetic accessibility"
 
-try:
-    from .farscore import predict_synthesizability, load_model
-    from .farscore_pretrain import pretrain_model
-    from .farscore_finetune import finetune_model
-except ImportError:
-    pass
-
-__all__ = [
-    '__version__',
-    'predict_synthesizability',
-    'load_model',
-    'pretrain_model',
-    'finetune_model',
-]
-
-def get_version():
-    return __version__
-
-def get_package_info():
+def get_model_info():
     return {
-        'name': 'farscore',
+        'name': 'farscore-model',
         'version': __version__,
-        'author': __author__,
-        'description': __description__,
+        'description': 'FARScore model package'
     }
