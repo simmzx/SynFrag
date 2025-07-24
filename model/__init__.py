f34f3d0d31938a7b49aa7c6a3782f450e0e864f2
Xiang Zhang <zhangxiang@simm.ac.cn>
2025-07-24 21:10:28 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -6,19 +6,22 @@ FARScore: A deep learning tool for predicting molecular synthetic accessibility
 -  (farscore.py)
 -  (farscore_pretrain.py)
 -  (farscore_finetune.py)
+-  (cli.py)
 -  (scripts/model/)
 -  (scripts/utils/)
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Xiang Zhang"
-__email__ = "776206454@qq.com"
+__email__ = "zhangxiang@simm.ac.cn"
 __description__ = "A deep learning tool based fragment assembly autoregressive pretrain for predicting molecular synthetic accessibility"
 
 try:
     from .farscore import predict_synthesizability, load_model
     from .farscore_pretrain import pretrain_model
     from .farscore_finetune import finetune_model
+
+    from . import cli
     
     from . import scripts
     
@@ -31,6 +34,7 @@ __all__ = [
     'load_model',
     'pretrain_model',
     'finetune_model',
+    'cli',
     'scripts',
 ]
 
